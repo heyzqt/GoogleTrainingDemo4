26e49872c517fa260117d0c000b1fab5dde9a4fa
zhangqing <zhangqing02@skyworth.com>
2017-12-04 14:59:50 +0800

    将Img图片加载到内存中计算出最接近reqWidth和reqHeight的width和height后再显示图片

--- app/src/main/java/com/heyzqt/googletrainingdemo4/MainActivity.java
+++ app/src/main/java/com/heyzqt/googletrainingdemo4/MainActivity.java
@@ -1,21 +1,56 @@
 package com.heyzqt.googletrainingdemo4;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private ImageView mImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		BitmapFactory.Options mBitmapOptions = new BitmapFactory.Options();
-		mBitmapOptions.inJustDecodeBounds = true;
-		System.out.println("before width = " + mBitmapOptions.outWidth + ",height = " + mBitmapOptions.outHeight);
-		BitmapFactory.decodeResource(getResources(), R.drawable.img, mBitmapOptions);
-		System.out.println("width = " + mBitmapOptions.outWidth);
-		System.out.println("height = " + mBitmapOptions.outHeight);
+		init();
+	}
+
+	private void init() {
+		mImageView = findViewById(R.id.imageview);
+		//get a picture which width and height is near to 100px
+		mImageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.img, 100, 100));
+	}
+
+	private int calculateInSampleSize(BitmapFactory.Options opt, int reqWidth, int reqHeight) {
+		int inSampleSize = 1;
+		int width = opt.outWidth;
+		int height = opt.outHeight;
+
+		if (width > reqWidth || height > reqHeight) {
+			final int halfWidth = width / 2;
+			final int halfHeight = height / 2;
+			while ((halfWidth / inSampleSize) > reqWidth && (halfHeight / inSampleSize) > reqHeight) {
+				inSampleSize *= 2;
+			}
+		}
+		return inSampleSize;
+	}
+
+	private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+			int reqWidth, int reqHeight) {
+		//get the dimensions of the picture
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeResource(res, resId, options);
+		int width = options.outWidth;
+		int height = options.outHeight;
+
+		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeResource(res, resId, options);
 	}
 }
