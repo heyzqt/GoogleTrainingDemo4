c13da16e9356734d958d67fcaa4f5bd7ee3ec357
zhangqing <zhangqing02@skyworth.com>
2017-12-23 09:50:18 +0800

    添加setSuitableBitmap()和calculateSampleSize()

--- app/src/main/java/com/heyzqt/googletrainingdemo4/MainActivity.java
+++ app/src/main/java/com/heyzqt/googletrainingdemo4/MainActivity.java
@@ -5,11 +5,14 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 	private ImageView mImageView;
+
+	private static final String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,41 @@ public class MainActivity extends AppCompatActivity {
 
 	private void init() {
 		mImageView = findViewById(R.id.imageview);
+		mImageView.setImageBitmap(setSuitableBitmap(getResources(), R.drawable.img,
+				100, 100));
+
+
 		//get a picture which width and height is near to 100px
-		mImageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.img, 100, 100));
+//		mImageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.img,
+// 100, 100));
+	}
+
+	private Bitmap setSuitableBitmap(Resources res, int resId, int reqWidth, int reqHeight) {
+		//Get the ImageView's dimensions
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeResource(res, resId, options);
+
+		//According the dimensions and reqWidth and reqHeight to calculate inSampleSize
+		int tempSampleSize = calculateSampleSize(options, reqWidth, reqHeight);
+
+		//ImageView set the bitmap, P.S. need to load the bitmap to the memory
+		options.inJustDecodeBounds = false;
+		options.inSampleSize = tempSampleSize;
+		return BitmapFactory.decodeResource(res, resId, options);
+	}
+
+	private int calculateSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		int outWidth = options.outWidth;
+		int outHeight = options.outHeight;
+		int sampleSize = 1;
+
+		while (outWidth / sampleSize > reqWidth && outHeight / sampleSize > reqHeight) {
+			sampleSize *= 2;
+		}
+
+		Log.i(TAG, "calculateSampleSize: sample size = " + sampleSize);
+		return sampleSize;
 	}
 
 	private int calculateInSampleSize(BitmapFactory.Options opt, int reqWidth, int reqHeight) {
@@ -33,7 +69,8 @@ public class MainActivity extends AppCompatActivity {
 		if (width > reqWidth || height > reqHeight) {
 			final int halfWidth = width / 2;
 			final int halfHeight = height / 2;
-			while ((halfWidth / inSampleSize) > reqWidth && (halfHeight / inSampleSize) > reqHeight) {
+			while ((halfWidth / inSampleSize) > reqWidth
+					&& (halfHeight / inSampleSize) > reqHeight) {
 				inSampleSize *= 2;
 			}
 		}
